b0c0ffcdc7729976a3ea0b2879d58362f8c3776b
unknown <azadmilad1993@gmail.com>
2023-06-21 23:55:32 +0330

    add suggestionSwiper

--- components/swiper/suggestionSwiper.jsx
+++ components/swiper/suggestionSwiper.jsx
@@ -12,45 +12,50 @@ import Image from "next/image";
 
 const SuggestionSwiper = ({ allProducts }) => {
   return (
-    // <div className="w-full h-80 cursor-pointer mb-5">
-    <Swiper
-      // slidesPerView="auto"
-      className=" w-full h-full rounded-2xl border"
-      breakpoints={{
-        0: {
-          slidesPerView: 3,
-          grid: {
-            rows: 2,
+    <div className="h-80 cursor-pointer mb-5">
+      <Swiper
+        slidesPerView="auto"
+        breakpoints={{
+          0: {
+            slidesPerView: 3,
+            grid: {
+              rows: 2,
+            },
           },
-        },
-        640: {
-          slidesPerView: 5,
-          grid: {
-            rows: 2,
+          640: {
+            slidesPerView: 5,
+            grid: {
+              rows: 2,
+            },
           },
-        },
-        1200: {
-          slidesPerView: 7,
-          grid: {
-            rows: 2,
+          1200: {
+            slidesPerView: 7,
+            grid: {
+              rows: 2,
+            },
           },
-        },
-      }}
-      modules={[Grid, Navigation]}
-      navigation={true}
-    >
-      {allProducts
-        .filter((products) => products.isSuggest == true)
-        .slice(0, 14)
-        .map((product) => (
-          <SwiperSlide>
-            <div className="flex flex-col justify-center items-center w-full text-center border ">
-              <Image src={product.thumbnail} width={100} height={100} />
-            </div>
-          </SwiperSlide>
-        ))}
-    </Swiper>
-    // </div>
+        }}
+        navigation={true}
+        modules={[Grid, Navigation]}
+        className="suggestionSwiper w-full h-full rounded-2xl border"
+      >
+        {allProducts
+          .filter((products) => products.isSuggest == true)
+          .slice(0, 14)
+          .map((product) => {
+            return (
+              <SwiperSlide>
+                <div className="flex flex-col justify-center items-center w-full text-center border ">
+                  <Image src={product.thumbnail} width={80} height={80} />
+                  <span className="font-bold lg:text-xs line-clamp-1 pt-2">
+                    {product.name}
+                  </span>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+      </Swiper>
+    </div>
   );
 };
 
