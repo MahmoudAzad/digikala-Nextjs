67f72710ec94a483456c3b6f58f20a388ba444a1
unknown <azadmilad1993@gmail.com>
2023-10-28 23:21:20 +0330

    single page debug and test

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -3,9 +3,25 @@ import SingleProductDesctop from "./singleProductDesctop";
 import SingleProductMobile from "./singleProductMobile";
 import SingleProductMenu from "@/components/menu/singleProductMenu";
 import { fetchSingleProduct } from "@/services/services";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
-const SingleProduct = async ({ params = {} }) => {
-  const singleProductData = await fetchSingleProduct(params.id);
+const SingleProduct = () => {
+  const { id } = useParams();
+  const [singleProductData, setSingleProductData] = useState();
+  useEffect(() => {
+    const fetchFunction = async () => {
+      const milad = await fetchSingleProduct(id);
+      setSingleProductData(milad);
+      console.log("milad => ", milad);
+      console.log("id => ", id);
+    };
+    fetchFunction();
+  }, [id]);
+
+  if (!singleProductData) {
+    return <h1>wait</h1>;
+  }
   return (
     <>
       <SingleProductDesctop
